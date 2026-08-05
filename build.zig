9bc5f61ca57e42ee3d4366a176515cdcd5b44444
Bernard Assan <mega.alpha100@gmail.com>
2026-08-05 15:38:29 +0000

    add Options struct for examples
    update tardy
    
    cleanup build.zig formatting
    
    Signed-off-by: Bernard Assan <mega.alpha100@gmail.com>

--- build.zig
+++ build.zig
@@ -15,10 +15,10 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const options = b.addOptions();
-    options.addOption(TlsImpl, "tls", tls);
+    const tls_option = b.addOptions();
+    tls_option.addOption(TlsImpl, "tls", tls);
 
-    const lib = b.addModule("secsock", .{
+    const secsock = b.addModule("secsock", .{
         .root_source_file = b.path("src/Secsock.zig"),
         .target = target,
         .optimize = optimize,
@@ -29,12 +29,20 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     }).module("tardy");
 
-    lib.addImport("tardy", tardy);
-    lib.addImport("options", options.createModule());
+    secsock.addImport("tardy", tardy);
+    secsock.addImport("options", tls_option.createModule());
 
     const check = b.step("check", "Check compilation errors");
 
+    const options: Options = .{
+        .optimize = optimize,
+        .target = target,
+        .tardy = tardy,
+        .secsock = secsock,
+    };
+
     switch (tls) {
+        // bearssl is always enabled by default
         .bearssl => if (b.lazyDependency("bearssl", .{
             .target = target,
             .optimize = optimize,
@@ -42,7 +50,7 @@ pub fn build(b: *std.Build) void {
             .BR_BE_UNALIGNED = false,
         })) |bearssl| {
             const bearssl_h = bearssl.module("bearssl.h");
-            lib.addImport("bearssl.h", bearssl_h);
+            secsock.addImport("bearssl.h", bearssl_h);
 
             const builder = bearssl.builder;
             const bearssl_check_step = &builder.top_level_steps.get(
@@ -51,23 +59,16 @@ pub fn build(b: *std.Build) void {
             check.dependOn(bearssl_check_step);
 
             const bearssl_lib = bearssl.artifact("bearssl");
-            lib.linkLibrary(bearssl_lib);
+            secsock.linkLibrary(bearssl_lib);
 
-            add_example(
-                b,
-                "bearssl",
-                target,
-                optimize,
-                tardy,
-                lib,
-            );
+            add_example(b, "bearssl", options);
         },
         .s2n_tls => if (b.lazyDependency("s2n_tls", .{
             .target = target,
             .optimize = optimize,
         })) |s2n_tls| {
             const s2n_h = s2n_tls.module("s2n.h");
-            lib.addImport("s2n.h", s2n_h);
+            secsock.addImport("s2n.h", s2n_h);
 
             const builder = s2n_tls.builder;
             const s2n_check_step = &builder.top_level_steps.get(
@@ -76,52 +77,58 @@ pub fn build(b: *std.Build) void {
             check.dependOn(s2n_check_step);
 
             const s2n_lib = s2n_tls.artifact("s2n");
-            lib.linkLibrary(s2n_lib);
+            secsock.linkLibrary(s2n_lib);
 
-            add_example(
-                b,
-                "s2n",
-                target,
-                optimize,
-                tardy,
-                lib,
-            );
+            add_example(b, "s2n", options);
         },
     }
+    add_example(b, "unsecured", options);
 }
 
-fn add_example(
-    b: *std.Build,
-    name: []const u8,
-    target: std.Build.ResolvedTarget,
-    optimize: std.lang.Optimize,
-    tardy_module: *std.Build.Module,
-    secsock_module: *std.Build.Module,
-) void {
+fn add_example(b: *std.Build, name: []const u8, options: Options) void {
     const mod = b.createModule(.{
-        .root_source_file = b.path(b.fmt("examples/{s}/main.zig", .{name})),
-        .target = target,
-        .optimize = optimize,
+        .root_source_file = b.path(b.fmt(
+            "examples/{s}/main.zig",
+            .{name},
+        )),
+        .target = options.target,
+        .optimize = options.optimize,
         .strip = false,
     });
-    mod.addImport("tardy", tardy_module);
-    mod.addImport("secsock", secsock_module);
+    mod.addImport("tardy", options.tardy);
+    mod.addImport("secsock", options.secsock);
 
     const example = b.addExecutable(.{
         .name = b.fmt("{s}", .{name}),
         .root_module = mod,
     });
 
-    const install_artifact = b.addInstallArtifact(example, .{});
+    const install_artifact = b.addInstallArtifact(
+        example,
+        .{},
+    );
     b.getInstallStep().dependOn(&install_artifact.step);
 
-    const build_step = b.step(b.fmt("{s}", .{name}), b.fmt("Build tardy example ({s})", .{name}));
+    const build_step = b.step(
+        b.fmt("{s}", .{name}),
+        b.fmt("Build tardy example ({s})", .{name}),
+    );
     build_step.dependOn(&install_artifact.step);
 
     const run_artifact = b.addRunArtifact(example);
     run_artifact.step.dependOn(&install_artifact.step);
 
-    const run_step = b.step(b.fmt("run_{s}", .{name}), b.fmt("Run tardy example ({s})", .{name}));
+    const run_step = b.step(
+        b.fmt("run_{s}", .{name}),
+        b.fmt("Run tardy example ({s})", .{name}),
+    );
     run_step.dependOn(&install_artifact.step);
     run_step.dependOn(&run_artifact.step);
 }
+
+const Options = struct {
+    target: std.Build.ResolvedTarget,
+    optimize: std.lang.Optimize,
+    tardy: *std.Build.Module,
+    secsock: *std.Build.Module,
+};
